aa303a090dc29ff4fbb5ea6037a9d5c855f04457
SebastianEscurra <sebastian_escurra@hotmail.com>
2023-07-24 17:46:51 -0300

    Modificamos la funcion Listar,Agregar,Modificar de Articulo

--- GestionFabricaZapatos/Negocio/ArticuloNegocio.cs
+++ GestionFabricaZapatos/Negocio/ArticuloNegocio.cs
@@ -10,17 +10,106 @@ namespace Negocio
 {
     public class ArticuloNegocio
     {
+        private AccesoDatos dato = new AccesoDatos();
         public List<Articulo> listar()
         {
-            return new List<Articulo>();
+            
+            try
+            {
+                List<Articulo> ListaArticulos = new List<Articulo>();
+                dato.setearConsulta("select a.Id,a.Nombre,a.Cantidad,a.PecioFabricacion,a.PrecioComercial,a.PrecioMayorista,a.IdTipo IdTipo,t.Descripcion Modelo,a.IdInsumos,i.Descripcion Insumos,a.IdSucursal,s.Descripcion Sucursal from Articulo a,Tipo t,Insumo i,Sucursal s where t.Id=a.IdTipo and i.Id=a.IdInsumos and s.Id=a.IdSucursal");
+                dato.ejecutarLectura();
+
+                while (dato.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+
+                    aux.Id = (int)dato.Lector["Id"];
+                    aux.Nombre =(string) dato.Lector["Nombre"];
+                    aux.Cantidad = (int)dato.Lector["Cantidad"];
+                    aux.PrecioFabricacion = (decimal)dato.Lector["PecioFabricacion"];
+                    aux.PrecioComercial = (decimal)dato.Lector["PrecioComercial"];
+                    aux.Preciomayorista = (decimal)dato.Lector["PrecioMayorista"];
+                    aux.Modelo = new Tipo();
+                    aux.Modelo.Id = (int)dato.Lector["Idtipo"];
+                    aux.Modelo.Descripcion = (string)dato.Lector["Modelo"];
+                    aux.Insumo = new Insumo();
+                    aux.Insumo.id = (int)dato.Lector["IdInsumos"];
+                    aux.Insumo.Descripcion =(string) dato.Lector["Insumos"];
+                    aux.Sucursal = new Sucursal();
+                    aux.Sucursal.Id = (int)dato.Lector["IdSucursal"];
+                    aux.Sucursal.Descripcion = (string)dato.Lector["Sucursal"];
+
+                    ListaArticulos.Add(aux);
+                }
+                return ListaArticulos;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                dato.cerrarConexion();
+            }
+            
         }
         public void agregar(Articulo nuevo)
         {
 
+            try
+            {
+                dato.setearConsulta("insert into Articulo values(@nombre,@cantidad,@precioFabricacion,@precioComercial,@precioMayorista,@IdTipo,@IdInsumos,@IdSucursal)");
+                dato.setearParametro("@nombre",nuevo.Nombre);
+                dato.setearParametro("@cantidad", nuevo.Cantidad);
+                dato.setearParametro("@precioFabricacion", nuevo.PrecioFabricacion);
+                dato.setearParametro("@precioComercial", nuevo.PrecioComercial);
+                dato.setearParametro("@precioMayorista", nuevo.PrecioComercial);
+                dato.setearParametro("@IdTipo",nuevo.Modelo.Id);
+                dato.setearParametro("@IdInsumos", nuevo.Insumo.id);
+                dato.setearParametro("@IdSucursal", nuevo.Sucursal.Id);
+                dato.ejecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                dato.cerrarConexion();
+            }
         }
         public void modificar(Articulo modificado)
         {
+            string alternarNombre = DateTime.Now.Millisecond.ToString();
+            try
+            {
+                dato.setearConsulta("update Articulo set Nombre=@nombre"+alternarNombre+ ",Cantidad=@cantidad"+alternarNombre+ ",PecioFabricacion=@fabricacion"+alternarNombre+ ",PrecioComercial=@comercial"+alternarNombre+ ",PrecioMayorista=@mayorista"+alternarNombre+ ",IdTipo=@idTipo"+alternarNombre+ ",IdInsumos=@idInsumos"+alternarNombre+ ",IdSucursal=@idSucursal"+alternarNombre+ " where id=@id"+alternarNombre+"");
+                dato.setearParametro("@nombre"+alternarNombre,modificado.Nombre);
+                dato.setearParametro("@cantidad" + alternarNombre, modificado.Cantidad);
+                dato.setearParametro("@fabricacion" + alternarNombre, modificado.PrecioFabricacion);
+                dato.setearParametro("@comercial" + alternarNombre, modificado.PrecioComercial);
+                dato.setearParametro("@mayorista" + alternarNombre, modificado.Preciomayorista);
+                dato.setearParametro("@idtipo" + alternarNombre, modificado.Modelo.Id);
+                dato.setearParametro("@idInsumos" + alternarNombre, modificado.Insumo.id);
+                dato.setearParametro("@idSucursal" + alternarNombre, modificado.Sucursal.Id);
+                dato.setearParametro("@id" + alternarNombre, modificado.Id);
 
+                dato.ejecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                dato.cerrarConexion();
+            }
         }
         public void eliminar(int id)
         {
--- GestionFabricaZapatos/Presentacion/Form1.Designer.cs
+++ GestionFabricaZapatos/Presentacion/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(52, 60);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvArticulos.Size = new System.Drawing.Size(672, 150);
+            this.dgvArticulos.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(66, 234);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(194, 234);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 2;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvArticulos);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvArticulos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
     }
 }
 
